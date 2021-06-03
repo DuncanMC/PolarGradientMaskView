2b669a1c1a53ad62bec1297b15b797b9000270d3
Duncan Champney <DChampney@wareto.com>
2021-06-03 17:45:48 -0400

    Initial Commit

---
+++ PolarGradientMaskView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PolarGradientMaskView
+//
+//  Created by Duncan Champney on 6/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
